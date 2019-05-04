05a912d550c03a8fe2e413c79b3a3f24362526a8
Joel Palmer <joelpalmer@gmail.com>
2019-05-04 15:40:41 -0500

    add push to bad singly linked list

--- src/first.rs
+++ src/first.rs
@@ -1,3 +1,5 @@
+use std::mem;
+
 struct List {
     head: Link,
 }
@@ -5,6 +7,18 @@ struct List {
 impl List {
     pub fn new() -> Self {
         List { head: Link::Empty }
+    }
+
+    pub fn push(&mut self, elem: i32) {
+        let new_node = Box::new(Node {
+            elem,
+            next: mem::replace(&mut self.head, Link::Empty),
+        });
+        self.head = Link::More(new_node);
+    }
+
+    pub fn pop(&mut self) -> Option<i32> {
+        /// TODO
     }
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,2 @@
 pub mod first;
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+
