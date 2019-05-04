870a97fae05097cec8e28fe1dcf18e195a2086a0
Joel Palmer <joelpalmer@gmail.com>
2019-05-04 16:45:32 -0500

    add test coverage

--- src/first.rs
+++ src/first.rs
@@ -4,6 +4,16 @@ struct List {
     head: Link,
 }
 
+enum Link {
+    Empty,
+    More(Box<Node>),
+}
+
+struct Node {
+    elem: i32,
+    next: Link,
+}
+
 impl List {
     pub fn new() -> Self {
         List { head: Link::Empty }
@@ -28,12 +38,30 @@ impl List {
     }
 }
 
-enum Link {
-    Empty,
-    More(Box<Node>),
-}
 
-struct Node {
-    elem: i32,
-    next: Link,
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn basics() {
+        let mut list = List::new();
+        // Check empty list
+        assert_eq!(list.pop(), None);
+        list.push(1);
+        list.push(2);
+        list.push(3);
+        // check removal
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+        // add more
+        list.push(4);
+        list.push(5);
+        // check removal
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), Some(4));
+        // final check
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), None);
+    }
 }
