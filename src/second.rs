3d35a5684cfd3520e78339d9d882dd7765e75eb5
Joel Palmer <joelpalmer@gmail.com>
2019-05-05 10:33:23 -0500

    work on more OK version of singly linked list

--- src/second.rs
+++ src/second.rs
@@ -4,10 +4,7 @@ pub struct List {
     head: Link,
 }
 
-enum Link {
-    Empty,
-    More(Box<Node>),
-}
+type Link = Option<Box<Node>>;
 
 struct Node {
     elem: i32,
@@ -16,34 +13,32 @@ struct Node {
 
 impl List {
     pub fn new() -> Self {
-        List { head: Link::Empty }
+        List { head: None }
     }
 
     pub fn push(&mut self, elem: i32) {
         let new_node = Box::new(Node {
             elem,
-            next: mem::replace(&mut self.head, Link::Empty),
+            next: self.head.take(),
         });
-        self.head = Link::More(new_node);
+        self.head = Some(new_node);
     }
 
     pub fn pop(&mut self) -> Option<i32> {
-        match mem::replace(&mut self.head, Link::Empty) {
-            Link::Empty => None,
-            Link::More(node) => {
-                self.head = node.next;
-                Some(node.elem)
-            }
-        }
+        self.head.take().map(|node| {
+            self.head = node.next;
+            node.elem
+        })
     }
 }
 
+
 impl Drop for List {
     fn drop(&mut self) {
-        let mut cur_link = mem::replace(&mut self.head, Link::Empty);
+        let mut cur_link = self.head.take();
 
-        while let Link::More(mut boxed_node) = cur_link {
-            cur_link = mem::replace(&mut boxed_node.next, Link::Empty);
+        while let Some(mut boxed_node) = cur_link {
+            cur_link = boxed_node.next.take();
         }
     }
 }
@@ -56,21 +51,21 @@ mod tests {
     #[test]
     fn basics() {
         let mut list = List::new();
-        // Check empty list
+// Check empty list
         assert_eq!(list.pop(), None);
         list.push(1);
         list.push(2);
         list.push(3);
-        // check removal
+// check removal
         assert_eq!(list.pop(), Some(3));
         assert_eq!(list.pop(), Some(2));
-        // add more
+// add more
         list.push(4);
         list.push(5);
-        // check removal
+// check removal
         assert_eq!(list.pop(), Some(5));
         assert_eq!(list.pop(), Some(4));
-        // final check
+// final check
         assert_eq!(list.pop(), Some(1));
         assert_eq!(list.pop(), None);
     }
