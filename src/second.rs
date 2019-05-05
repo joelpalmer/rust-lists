ae64e5523f275ca113e39983578d81116bbece55
Joel Palmer <joelpalmer@gmail.com>
2019-05-05 12:13:22 -0500

    implement IntoIter

--- src/second.rs
+++ src/second.rs
@@ -41,8 +41,11 @@ impl<T> List<T> {
             &mut node.elem
         })
     }
-}
 
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
+}
 
 impl<T> Drop for List<T> {
     fn drop(&mut self) {
@@ -54,10 +57,33 @@ impl<T> Drop for List<T> {
     }
 }
 
+pub struct IntoIter<T>(List<T>);
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        // access fields of tuple numerically
+        self.0.pop()
+    }
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn into_iter() {
+        let mut list = List::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        let mut iter = list.into_iter();
+        assert_eq!(iter.next(), Some(3));
+        assert_eq!(iter.next(), Some(2));
+        assert_eq!(iter.next(), Some(1));
+        assert_eq!(iter.next(), None);
+    }
 
     #[test]
     fn basics() {
