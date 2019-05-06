9df12e18f78ef739e16e8acf0d78f2188683f71e
Joel Palmer <joelpalmer@gmail.com>
2019-05-06 11:28:58 -0500

    impl drop

--- src/persistent_singly_linked.rs
+++ src/persistent_singly_linked.rs
@@ -53,6 +53,19 @@ impl<'a, T> Iterator for Iter<'a, T> {
     }
 }
 
+impl<T> Drop for List<T> {
+    fn drop(&mut self) {
+        let mut head = self.head.take();
+        while let Some(node) = head {
+            if let Ok(mut node) = Rc::try_unwrap(node) {
+                head = node.next.take();
+            } else {
+                break;
+            }
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
