6d3d82a13c8689cd5d68153636239b9c98010b5a
Joel Palmer <joelpalmer@gmail.com>
2019-05-07 09:22:32 -0500

    start bad but safe doubly linked deque

---
+++ src/bad_safe_doubly_linked_deque.rs
@@ -0,0 +1,16 @@
+use std::rc::Rc;
+use std::cell::{RefCell, Ref};
+
+pub struct List<T> {
+    head: Link<T>,
+    tail: Link<T>,
+}
+
+type Link<T> = Option<Rc<RefCell<Node<T>>>>;
+
+struct Node<T> {
+    elem: T,
+    next: Link<T>,
+    prev: Link<T>,
+}
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 pub mod bad_singly_linked;
 pub mod ok_singly_linked;
 pub mod persistent_singly_linked;
+pub mod bad_safe_doubly_linked_deque;
